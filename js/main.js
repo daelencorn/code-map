2598e32026be9e16b3b0472fa67f5af276e69487
daelencorn <daelencorn@gmail.com>
2015-06-15 22:16:14 -0700

    Reset map zoom

--- js/main.js
+++ js/main.js
@@ -132,6 +132,7 @@ var locations = [
   var address = document.getElementById('address').value;
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
+      map.setZoom(6);
       map.setCenter(results[0].geometry.location);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
